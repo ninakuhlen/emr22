1284d9e29e56d38ac7a3ea01eab5350b6d8cd309
ProfJust <olaf.just@w-hs.de>
2022-01-31 09:13:29 +0100

    startversion

--- _ur3/testMoveIt.py
+++
@@ -1,7 +0,0 @@
-#testMoveIt
-
-from moveit_commander import MoveGroupCommander
-cmd = MoveGroupCommander(ur3)
-cmd.set_pose_target(pose)  # to move the endeffector into a specific pose, OR
-cmd.set_joint_value_target(joint_values) # if you know the positions the joints should be in
-cmd.go()
