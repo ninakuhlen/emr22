d17248039ebf6f27c70cdafebf725809922e045d
Prof. Just <olaf.just@w-hs.de>
2022-01-31 10:02:11 +0100

    neu

---
+++ nodes/01_listener_talker/talker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# --- talker.py ------
+# Version vom 18.10.2021 by OJ
+# -----------------------------
+import rospy  # Standard Bibliothek für ROS mit Python
+from std_msgs.msg import String  # vordefinierte Message
+
+
+def talker():
+    # Publisher wirt vereinbart message name, typ, ..
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    # Node wird vereinbart
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1)  # 1hz
+    i = 0
+    while not rospy.is_shutdown():
+        hello_str = "WHS - Hello python world %d" % i
+        i = i + 1
+        rospy.loginfo(hello_str)  # Ausgabe Shell
+        pub.publish(hello_str)  # Ausgabe im ROS
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        rospy.loginfo(" Fehler im ROS ")
+        #  pass
