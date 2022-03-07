954305113bdfd5559ac50cfd5ae52e9122659fd0
ProfJust <olaf.just@w-hs.de>
2022-03-07 18:40:24 +0100

    yep

--- scripts/starthilfe_ur5.py
+++ scripts/starthilfe_ur5.py
@@ -38,7 +38,7 @@ class MainWindow(QWidget):
 
         # --- Window konfigurieren und starten
         self.setGeometry(300, 300, 600, 400)
-        self.setWindowTitle('EMR22 - main-control-gui')
+        self.setWindowTitle('EMR22 - Starthilfe UR5e ')
         self.show()
 
     # --- Die  Slot-Methoden ---
@@ -54,53 +54,9 @@ class MainWindow(QWidget):
     def slot_pick_place_depth_cam(self):
         os.system('gnome-terminal -- bash -c "rosrun emr22 pick_and_place_collision_depth_cam.py; exec bash"')
 
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     mw = MainWindow()
     sys.exit(app.exec_())
-
-
-"""# --- Slider erstellen -----
-        self.mySlider = QSlider(Qt.Horizontal, self)
-
-        #mySlider = mySliderClass()
-        self.mySlider.setFocusPolicy(Qt.NoFocus)
-        self.mySlider.setGeometry(30, 40, 180, 30) #x,y,w,h
-        self.mySlider.setValue(20)
-
-        # --- LCD konstruieren -----
-        self.myLcd=QLCDNumber(2, self)
-        self.myLcd.setGeometry(60, 100, 80, 50) #x,y,w,h
-        self.myLcd.display(20)
-
-        # Verbinden des Signals valueChanged
-        # mit der Slot-Funktion myLcd.display
-        self.mySlider.valueChanged[int].connect(self.myLcd.display)
-
-        #--- zwei PushButtons
-        myPBmore = QPushButton(self)
-        myPBmore.setText('>')
-        myPBmore.setGeometry(0, 0, 40, 40) #x,y,w,h
-        myPBmore.clicked.connect(self.plus)
-
-        self.myPBless = QPushButton(self)
-        self.myPBless.setText('<')
-        self.myPBless.setGeometry(180, 0, 40, 40) #x,y,w,h
-        self.myPBless.clicked.connect(self.minus)
-
-
-        #--- Window konfigurieren
-        self.setGeometry(300, 300, 280, 170)
-        self.setWindowTitle('Robotik Bocholt - Slider LCD')
-        self.show()
-
-    #--- Die beiden Slot-Methoden
-    def plus(self):
-        wert =self.mySlider.value()  #Slider Wert holen
-        wert =self.wert+1
-        mySlider.setValue(wert) #Slider Wert setzen
-    def minus(self):
-        wert = self.mySlider.value()
-        wert =wert-1
-        self.mySlider.setValue(wert)"""
