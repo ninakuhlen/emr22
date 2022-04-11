d60ec40d6975cfefe799bfad767050bec5b1761e
ProfJust <olaf.just@w-hs.de>
2022-04-11 10:26:11 +0200

    Plenum UE01

--- nodes/ue01_ur5_gazebo_qt_slider/ur5_gazebo_qt_slider.py
+++ nodes/ue01_ur5_gazebo_qt_slider/ur5_gazebo_qt_slider.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # --- ur5_gazebo_qt_slider.py ------
 # Qt-Gui with Slider to move UR5 in Gazebo
-# Version vom 30.3.2022 by OJ
+# Version vom 11.4.2022 by OJ
 # ----------------------------
 # usage:
 # $ roslaunch emr22 ur5_gazebo_pid.launch
@@ -21,7 +21,8 @@ from std_msgs.msg import Float64
 
 # Vollstaendiger Pfad der Datei zum Speichern der Positionen, "~" funkt nicht
 filename = "/home/oj/ws_moveit/src/emr22/nodes/ue01_ur5_gazebo_qt_slider/pose_ur5.txt"
-######## oj gegen ihren Username ersetzen !!! ##################
+# ####### oj gegen ihren Username ersetzen !!! ##################
+
 
 class UIClass(QWidget):
     def __init__(self):  # Konstrukor
@@ -41,61 +42,60 @@ class UIClass(QWidget):
 
         self.path = [[0.0, 0.0]]  # Initial Koordinaten
 
-    def initUI(self):
-        # Instanziierung der Widgets
-        self.lblInfoX = QLabel('Wrist 1  * 10')
+    def initUI(self):    # GUI - Instanziierung der Widgets
+        self.lblInfo1 = QLabel('Wrist 1  * 10')
         LCDstartWert = 0
-        self.lcdX = QLCDNumber(self)
-        self.lcdX.display(LCDstartWert)
-        
-        self.sldX = QSlider(Qt.Horizontal, self)
-        self.sldX.setMaximum(30)
-        self.sldX.setMinimum(-30)
-        self.sldX.setValue(LCDstartWert)
 
-        self.pbLessX = QPushButton('<')
-        self.pbMoreX = QPushButton('>')
+        # --- Wrist 1---
+        self.lcd1 = QLCDNumber(self)
+        self.lcd1.display(LCDstartWert)
+        self.sld1 = QSlider(Qt.Horizontal, self)
+        self.sld1.setMaximum(30)
+        self.sld1.setMinimum(-30)
+        self.sld1.setValue(LCDstartWert)
+        self.pbLess1 = QPushButton('<')
+        self.pbMore1 = QPushButton('>')
 
+        # --- Wrist 2 ---
         self.lblInfo2 = QLabel('Wrist 2  * 10')
         self.lcd2 = QLCDNumber(self)
         self.lcd2.display(LCDstartWert)
-        
         self.sld2 = QSlider(Qt.Horizontal, self)
         self.sld2.setMaximum(30)
         self.sld2.setMinimum(-30)
         self.sld2.setValue(LCDstartWert)
-
         self.pbLess2 = QPushButton('<')
         self.pbMore2 = QPushButton('>')
 
+        # --- Buttons ---
         self.pbGo = QPushButton(' Go Home ')
         self.pbStore = QPushButton(' Store Pose ')
         self.pbRead = QPushButton(' Read  Pose ')
-    
+
         self.lblStatus = QLabel('Status - Ausgabe')
 
         # BOX-Layout mit Widgets füllen
         vbox = QVBoxLayout()
+        # ---- Wrist1 -----
         #  0.Reihe - Label
         hbox = QHBoxLayout()
-        hbox.addWidget(self.lblInfoX)
+        hbox.addWidget(self.lblInfo1)
         vbox.addLayout(hbox)
         # 1.Reihe
         hbox = QHBoxLayout()
-        hbox.addWidget(self.lcdX)
+        hbox.addWidget(self.lcd1)
         vbox.addLayout(hbox)
         # 2.Reihe
         hbox = QHBoxLayout()
-        hbox.addWidget(self.sldX)
+        hbox.addWidget(self.sld1)
         vbox.addLayout(hbox)
-
         # 3.Reihe
         hbox = QHBoxLayout()
-        hbox.addWidget(self.pbLessX)
-        hbox.addWidget(self.pbMoreX)
+        hbox.addWidget(self.pbLess1)
+        hbox.addWidget(self.pbMore1)
         vbox.addLayout(hbox)
 
-
+        # ---- Wrist2 -----
         #  0.Reihe - Label
         hbox = QHBoxLayout()
         hbox.addWidget(self.lblInfo2)
@@ -136,10 +136,10 @@ class UIClass(QWidget):
         self.show()
 
         # Signal und Slot verbinden
-        self.sldX.valueChanged.connect(self.lcdX.display)
-        self.sldX.valueChanged.connect(self.SlotPublish)  # publish to ROS
-        self.pbLessX.clicked.connect(self.SlotKlickX)
-        self.pbMoreX.clicked.connect(self.SlotKlickX)
+        self.sld1.valueChanged.connect(self.lcd1.display)
+        self.sld1.valueChanged.connect(self.SlotPublish)  # publish to ROS
+        self.pbLess1.clicked.connect(self.SlotKlick1)
+        self.pbMore1.clicked.connect(self.SlotKlick1)
 
         self.sld2.valueChanged.connect(self.lcd2.display)
         self.sld2.valueChanged.connect(self.SlotPublish)  # publish to ROS
@@ -149,19 +149,19 @@ class UIClass(QWidget):
         self.pbStore.clicked.connect(self.SlotStorePosition)
         self.pbRead.clicked.connect(self.SlotReadPosition)
 
-    def SlotKlickX(self):
+    def SlotKlick1(self):  # Wrist1 - Button < oder > gepusht
         sender = self.sender()
         self.lblStatus.setText(' X ' + sender.text() + ' was pressed')
         if sender.text() == '<':
-            wert = self.sldX.value()
+            wert = self.sld1.value()
             wert = wert-1
-            self.sldX.setValue(wert)
+            self.sld1.setValue(wert)
         else:
-            wert = self.sldX.value()
+            wert = self.sld1.value()
             wert = wert+1
-            self.sldX.setValue(wert)
+            self.sld1.setValue(wert)
 
-    def SlotKlick2(self):
+    def SlotKlick2(self):   # Wrist2 - Button < oder > gepusht
         sender = self.sender()
         self.lblStatus.setText(' X ' + sender.text() + ' was pressed')
         if sender.text() == '<':
@@ -175,24 +175,27 @@ class UIClass(QWidget):
 
     def SlotGoHome(self):
         self.lblStatus.setText(' Go Home Button klicked ')
-        self.pos_wrist1_pub.publish(0.0)
-        self.pos_wrist2_pub.publish(0.0)
+        self.sld1.setValue(0)
+        self.sld2.setValue(0)
 
     def SlotPublish(self):
-        self.wrist1_msg = self.sldX.value()
+        self.lblStatus.setText(' all topics publisht ')
+        self.wrist1_msg = self.sld1.value()
         self.pos_wrist1_pub.publish(float(self.wrist1_msg)/10.0)
         self.wrist2_msg = self.sld2.value()
         self.pos_wrist2_pub.publish(float(self.wrist2_msg)/10.0)
 
     def SlotStorePosition(self):
+        self.lblStatus.setText(' Pose stored to file')
         fobj = open(filename, 'w')
-        write_str = "[" + str(self.sldX.value()) + ","\
+        write_str = "[" + str(self.sld1.value()) + ","\
                     + str(self.sld2.value())\
                     + "] \n"
         fobj.write(write_str)
         fobj.close()
  
     def SlotReadPosition(self):
+        self.lblStatus.setText(' Pose read from file ')
         # Den vorgegebenen Pfad einlesen, jede Zeile ein Goal
         with open(filename, 'r') as fin:
             for line in fin:
@@ -200,10 +203,9 @@ class UIClass(QWidget):
         del self.path[0]  # [0, 0] entfernen (ertes Element )
         rospy.loginfo(str(self.path))
         # setze Slider mit den Werten aus der Datei
-        self.sldX.setValue(self.path[0][0])
+        self.sld1.setValue(self.path[0][0])
         self.sld2.setValue(self.path[0][1])
 
-    
 
 if __name__ == '__main__':
     try:
