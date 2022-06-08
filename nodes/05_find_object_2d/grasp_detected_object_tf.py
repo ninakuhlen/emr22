6622c08f378a8a2a9c53496d21b105d81f0eeb27
Prof. Just <olaf.just@w-hs.de>
2022-06-08 16:13:22 +0200

    Update grasp_detected_object_tf.py

--- nodes/05_find_object_2d/grasp_detected_object_tf.py
+++ nodes/05_find_object_2d/grasp_detected_object_tf.py
@@ -1,99 +1,138 @@
-#!/usr/bin/env python3
-# EMR22 - ProfJust
-# based on ROS Developers LIVE-Class #59: MoveIt! Robot Perception
-# https://www.youtube.com/watch?v=E71pb2H4VDQ
-#----------------------------------------------------------------------
-# Software License Agreement (BSD License)
+#!/usr/bin/env python
+# grasp_detected_object_tf.py
+# 
+# ------------------------------------
+# edited WHS, OJ , 8.6.2022 #
+# -------------------------------------
+# Pick and Place
+# in Python mit der move_group_api
+# und Kollsionsverhütung
+# -----------------------------------------
 #
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: 
-
+# http://docs.ros.org/en/jade/api/moveit_ros_planning_interface/html/classmoveit_1_1planning__interface_1_1MoveGroup.html
+# -----------------------------------------
+# usage
+#   ...
+# ----------------------------------------------------------------
 import sys
+import copy
 import rospy
 import moveit_commander
+import moveit_msgs.msg
 import geometry_msgs.msg
+from math import pi
+import numpy as np  # für deg2rad
 from tf import TransformListener
 
+
+def wait_for_state_update(box_name, scene, box_is_known=False,
+                          box_is_attached=False, timeout=4):
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+        attached_objects = scene.get_attached_objects([box_name])
+        is_attached = len(attached_objects.keys()) > 0
+        is_known = box_name in scene.get_known_object_names()
+        if (box_is_attached == is_attached) and (box_is_known == is_known):
+            return True
+        rospy.sleep(0.1)
+        seconds = rospy.get_time()
+    return False
+
+
+# First initialize moveit_ Command and rospy nodes:
 moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('move_group_interface_emr22', anonymous=True)
+rospy.init_node('move_group_python_interface_tutorial',
+                anonymous=True)
+# Instantiate the robot commander object,
+# which is used to control the whole robot
 robot = moveit_commander.RobotCommander()
 
+# Instantiate the MoveGroupCommander object.
+group_name = "ur3_arm"
+group = moveit_commander.MoveGroupCommander(group_name)
+group_name_gripper = "gripper"
+group_gripper = moveit_commander.MoveGroupCommander(group_name_gripper)
 
-input("confirm moving ur3_arm to home position")
-# Put the arm in the home position
-arm_group = moveit_commander.MoveGroupCommander("ur3_arm")
-arm_group.set_named_target("home")
-plan1 = arm_group.go()
+# Create a Publisher.
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
 
-#  1)  put the arm to first grasping position
-input("confirm moving ur3_arm to first grasping position")
-pose_target = geometry_msgs.msg.Pose()
-pose_target.orientation.w = 0.5
-pose_target.orientation.x = -0.5
-pose_target.orientation.y = 0.5
-pose_target.orientation.z = -0.5
-pose_target.position.x = 1.0
-pose_target.position.y = 1.0
-pose_target.position.z = 1.0
-arm_group.set_pose_target(pose_target)
-plan1 = arm_group.go()
+# ##### Getting Basic Information ###############
+# We can get the name of the reference frame for this robot:
+planning_frame = group.get_planning_frame()
+# print("= Reference frame: %s" % planning_frame)
 
-#  2)  put the arm 15cm higher
-input("confirm moving ur3_arm to first grasping position +15cm")
-pose_target.position.z += 0.15
-arm_group.set_pose_target(pose_target)
-plan1 = arm_group.go()
+# We can also print the name of the end-effector link for this group:
+eef_link = group.get_end_effector_link()
+# print("= End effector: %s" % eef_link)
 
-# get the pose of object_X  from Camera
+# We can get a list of all the groups in the robot:
+group_names = robot.get_group_names()
+# print("= Robot Groups:", robot.get_group_names())
+
+# Sometimes for debugging it is useful to print the entire state of the
+# robot:
+# print("= Printing robot state")
+# print(robot.get_current_state())
+# print("")
+
+# create tf-listener
 t = TransformListener()
-if t.frameExists("/world") and t.frameExists("/object_8"):
-    (translation, rotation) = t.lookupTransform("world", "object_8", rospy.Time())
+
+
+# ---- 1. Move to home position ----
+input("confirm moving ur3_arm to home position")
+joint_goal = group.get_named_target_values("home")
+group.go(joint_goal, wait=True)
+# print("Reached Joint Goal Home", joint_goal)
+
+# --- 2. Open the gripper
+# input("\n Open Gripper => Enter")
+joint_gripper = group_gripper.get_current_joint_values()
+print("Gripper Angle is", joint_gripper)
+joint_gripper[0] = 0.001  # complete open is 0.0007  closed is pi/4
+group_gripper.go(joint_gripper, wait=True)
+group_gripper.stop()
+
+# --- 3. Place the TCP above the box
+input("confirm moving ur3_arm to first grasping position")
+pose_goal = group.get_current_pose()
+pose_goal.pose.position.x = 0.3
+pose_goal.pose.position.y = 0.5
+pose_goal.pose.position.z = 0.18
+group.set_pose_target(pose_goal)
+plan = group.plan()
+sucess = group.go(wait=True)
+print("suc?", sucess)
+group.stop()
+group.clear_pose_targets()
+# print("Reached TCP Goal above blue box", joint_goal)
+
+# --- 4. get the pose of object_X  from Camera
+print("looking for object_9")
+
+if t.frameExists("/world") and t.frameExists("/object_9"):
+    (translation, rotation) = t.lookupTransform("world", "object_9", rospy.Time())
     print("detected object position")
     print(t)
     print(translation)
 else:
+    print("no bject_9 found => exit() ")
     exit()
 
-
-#  3)  put the arm to object
-input("confirm moving ur3_arm to detected object position")
-pose_target = geometry_msgs.msg.Pose()
-pose_target.orientation.w = 0.5
-pose_target.orientation.x = -0.5
-pose_target.orientation.y = 0.5
-pose_target.orientation.z = -0.5
-pose_target.position = translation  # from tf-Tree
-pose_target.position.z -= 0.15  # 15cm higher than object
-arm_group.set_pose_target(pose_target)
-plan1 = arm_group.go()
+# --- 5. go to object position
+pose_goal = group.get_current_pose()
+pose_goal.pose.position  = translation  # from tf-Tree
+pose_goal.pose.position.z -= 0.2  # 20cm higherr
+group.set_pose_target(pose_goal)
+plan = group.plan()
+sucess = group.go(wait=True)
+print("suc?", sucess)
+group.stop()
+group.clear_pose_targets()
 
 
+# --- at the end -----
+scene.remove_world_object(box_name)
