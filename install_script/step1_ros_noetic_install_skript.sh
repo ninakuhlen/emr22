e9da30e7b7b6eec9fe40a34607ae823db92afe16
Prof. Just <olaf.just@w-hs.de>
2022-06-09 13:24:33 +0200

    Update step1_ros_noetic_install_skript.sh

--- install_script/step1_ros_noetic_install_skript.sh
+++ install_script/step1_ros_noetic_install_skript.sh
@@ -17,6 +17,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-noetic-desktop-full
+sudo apt-get install python3-catkin-tools
 
 
 echo -e "\033[34m ---------- Konfiguriere .bashrc ------------ \033[0m "
