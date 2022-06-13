516f86bbee327901c0a439320290c8f79f1457e8
Prof. Just <olaf.just@w-hs.de>
2022-06-13 13:52:05 +0200

    mit numpy eul2quat

--- nodes/05_find_object_2d/fixed_tf_broadcaster.py
+++ nodes/05_find_object_2d/fixed_tf_broadcaster.py
@@ -1,40 +1,14 @@
 #!/usr/bin/env python3
-
 # http://wiki.ros.org/tf/Tutorials/Adding%20a%20frame%20%28Python%29
 # usage:   $ rosrun tf tf_echo world object_8
+# -----------------------------------------------------------------
+# Version vom 13.6.22 mit numpy eul2quat
 
 import rospy
 import tf
 import numpy as np  # Scientific computing library for Python
 
 
-def eul2quat(roll, pitch, yaw):
-    """
-        https://automaticaddison.com/how-to-convert-euler-angles-to-quaternions-using-python/
-        Convert an Euler angle to a quaternion.
-
-        Input
-            :param roll: The roll (rotation around x-axis) angle in radians.
-            :param pitch: The pitch (rotation around y-axis) angle in radians.
-            :param yaw: The yaw (rotation around z-axis) angle in radians.
-
-        Output
-            :return qx, qy, qz, qw: The orientation in quaternion
-            [x,y,z,w] format
-    """
-    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2)
-    - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-
-    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-    + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-
-    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-    - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-
-    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2)
-    + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
- 
-    return [qx, qy, qz, qw]
 
 
 if __name__ == '__main__':
@@ -46,10 +20,13 @@ if __name__ == '__main__':
         # vorher messen in Meter
         # x, y, z    67cm hohes Stativ
         # quaternion?
-        br.sendTransform((0.0, 0.0, 0.67),
-                         (eul2quat(0.0, 0.78, 0.0)),
+
+        # UR5e 13_06_22
+        br.sendTransform((1.30, 0.04, 0.70),
+                         (tf.transformations.quaternion_from_euler(0.0 , 0.5, 3.14)),
                          rospy.Time.now(),
                          "camera_link",
                          "world")
-       
+
+        
         rate.sleep()
