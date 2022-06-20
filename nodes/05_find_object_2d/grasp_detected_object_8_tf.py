db686dbd0d2f694f0c1b8b295eb049dca01ec8b6
Prof. Just <olaf.just@w-hs.de>
2022-06-20 14:50:15 +0200

    Update grasp_detected_object_8_tf.py

--- nodes/05_find_object_2d/grasp_detected_object_8_tf.py
+++ nodes/05_find_object_2d/grasp_detected_object_8_tf.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python
-# pick_and_place_collision_v2.py
-# V3 ohne TCP-Goal und IK
+#!/usr/bin/env python3
+# grasp_detected_object_tf.py
+# 
 # ------------------------------------
-# edited WHS,, 30.5.2022 #
+# edited WHS, OJ , 20.6.2022 #
 # -------------------------------------
-# Pick and Place
+# Pick and Place für den realen UR3
 # in Python mit der move_group_api
-# und Kollsionsverhütung
+# und Kollsionsverhütung mit der Realsense 3D-Kamera
 # -----------------------------------------
-# basiert auf dem Code
-# https://roboticscasual.com/ros-tutorial-pick-and-place-task-with-the-moveit-c-interface/
+#
+# http://docs.ros.org/en/jade/api/moveit_ros_planning_interface/html/classmoveit_1_1planning__interface_1_1MoveGroup.html
 # -----------------------------------------
 # usage
-#   $1 roslaunch ur5_gripper_moveit_config demo_gazebo_pick_and_place.launch
-# 
-#   $2 rosrun emr22 pick_and_place.....py
+#   ...
 # ----------------------------------------------------------------
 import sys
 import copy
@@ -22,9 +20,9 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from math import pi
 import numpy as np  # für deg2rad
-
+from math import pi
+import tf
 
 def wait_for_state_update(box_name, scene, box_is_known=False,
                           box_is_attached=False, timeout=4):
@@ -55,15 +53,19 @@ group = moveit_commander.MoveGroupCommander(group_name)
 group_name_gripper = "gripper"
 group_gripper = moveit_commander.MoveGroupCommander(group_name_gripper)
 
-# Create a Publisher.
-display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                               moveit_msgs.msg.DisplayTrajectory,
-                                               queue_size=20)
+# Create a Publisher
+display_trajectory_publisher = rospy.Publisher(
+    '/move_group/display_planned_path',
+    moveit_msgs.msg.DisplayTrajectory,
+    queue_size=20)
+
+# create tf-listener
+listener = tf.TransformListener()
 
 # ##### Getting Basic Information ###############
 # We can get the name of the reference frame for this robot:
 planning_frame = group.get_planning_frame()
-print("= Reference frame: %s" % planning_frame)
+# print("= Reference frame: %s" % planning_frame)
 
 # We can also print the name of the end-effector link for this group:
 eef_link = group.get_end_effector_link()
@@ -71,179 +73,123 @@ print("= End effector: %s" % eef_link)
 
 # We can get a list of all the groups in the robot:
 group_names = robot.get_group_names()
-print("= Robot Groups:", robot.get_group_names())
 
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print("= Printing robot state")
-# print(robot.get_current_state())
-print("")
+# ---- 1. Move to home position ----
+input("confirm moving ur3_arm to home position")
+joint_goal = group.get_named_target_values("home")
+group.go(joint_goal, wait=True)
 
-# Create an object for PlanningSceneInterface.
-print("=== Adding Desktop Plate to Planning Scene  ===")
+# --- 2. get the pose of object_X  from Camera
+print("looking for object_8 ==>")
+(trans, rot) = listener.lookupTransform('/world', '/object_8', rospy.Time())
+print("detected object position")
+print(trans)
+
+# --- 3. Object in Scene eintragen
+# Desktop Plate is not needed because of Depth Cam, Yes!
+# But Gripper does not go to Desktop then =>
+# Add the object to the planning scene and deactivate collision checking
+print("=== Adding object to Planning Scene  ===")
 scene = moveit_commander.PlanningSceneInterface()
 rospy.sleep(1.0)  # Wichtig! ohne Pause funkts nicht
 box_pose = geometry_msgs.msg.PoseStamped()
 box_pose.header.frame_id = robot.get_planning_frame()
 box_pose.pose.orientation.w = 1.0
-box_pose.pose.position.z = -0.22  # 0.2 funkt, 0.25 zu tief => Arm Berührt Tischplatte
-box_name = "desktop"
-scene.add_box(box_name, box_pose, size=(2.0, 2.0, 0.05))
+box_pose.pose.position.x = trans[0]
+box_pose.pose.position.y = trans[1]
+box_pose.pose.position.z = trans[2]
+box_name = "object"
+scene.add_box(box_name, box_pose, size=(0.08, 0.08, 0.1))
 rospy.loginfo(wait_for_state_update(box_name, scene, box_is_known=True))
 
-# ---- 1. Move to home position ----
-# input(" Go to Home Position => Enter")
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = 0.001           # shoulder pan
-joint_goal[1] = -pi/2       # shoulder lift
-joint_goal[2] = pi/2        # elbow
-joint_goal[3] = -pi/2       # wrist1
-joint_goal[4] = -pi/2       # wrist2
-joint_goal[5] = 0.001          # wrist3
-group.go(joint_goal, wait=True)
-print("Reached Joint Goal Home", joint_goal)
+# --- 4. go to object position
+# rosrun tf tf_echo world robotiq_85_left_finger_link
+# trans = [0.36, 0.08, 0.3]  # Test Position
+print("translation is ", trans)
+pose_goal = group.get_current_pose()
+pose_goal.pose.position.x = trans[0] - 0.04  # from tf-Tree
+pose_goal.pose.position.y = trans[1] - 0.02  # from tf-Tree
+pose_goal.pose.position.z = trans[2]  # from tf-Tree
+pose_goal.pose.position.z = 0.35  # 35cm high
 
-# --- 2. Open the gripper
-# input("\n Open Gripper => Enter")
-joint_gripper = group_gripper.get_current_joint_values()
-print("Gripper Angle is", joint_gripper)
-joint_gripper[0] = 0.001  # complete open is 0.0007  closed is pi/4
-group_gripper.go(joint_gripper, wait=True)
-group_gripper.stop()
+# pose_goal.orientation = [1.0, 1.0, 1.0, 1.0 ] # quaternion
+# Drehwinkel um den Vektor ist w => w = 7101 =>  pi/4 = 0.7853..
+print(" going to pose", pose_goal.pose.position)
+print(" going to orientation quaternion ", pose_goal.pose.orientation)
+# pose_goal.pose.orientation.w = 0
+quaternion = (
+    pose_goal.pose.orientation.x,
+    pose_goal.pose.orientation.y,
+    pose_goal.pose.orientation.z,
+    pose_goal.pose.orientation.w)
+euler_goal_pose_orientation = tf.transformations.euler_from_quaternion(quaternion) 
+print(" going to orientation euler", euler_goal_pose_orientation)
 
-# --- 3. Place the TCP above the blue box
-# input(" Go close to blue box => Enter")
-# ---> Goal with Jointangles
-# abgelesen nach Handfahrt
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = np.deg2rad(-74)       # shoulder pan
-joint_goal[1] = np.deg2rad(-84)      # shoulder lift
-joint_goal[2] = np.deg2rad(98)      # elbow
-joint_goal[3] = np.deg2rad(-104)     # wrist1
-joint_goal[4] = np.deg2rad(-90)      # wrist2
-joint_goal[5] = np.deg2rad(60)       # wrist3
-group.go(joint_goal, wait=True)
-print("Reached TCP Goal above blue box", joint_goal)
+quaternion = tf.transformations.quaternion_from_euler(
+    euler_goal_pose_orientation[0],
+    euler_goal_pose_orientation[1],
+    euler_goal_pose_orientation[2] + pi/4,
+    axes='sxyz'  )
+
+euler_goal_pose_orientation = tf.transformations.euler_from_quaternion(quaternion) 
+print(" going to orientation euler neu", euler_goal_pose_orientation)
+
+# WICHTIG!
+pose_goal.pose.orientation.x = quaternion[0]
+pose_goal.pose.orientation.y = quaternion[1]
+pose_goal.pose.orientation.z = quaternion[2]
+pose_goal.pose.orientation.w = quaternion[3]
 
 
-# --- 4. Move the TCP close to the object 0.2m down
-# ---> TCP Goal with Orientation
-input(" Go to grip-position => Enter")
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = np.deg2rad(-74)       # shoulder pan
-joint_goal[1] = np.deg2rad(-81)      # shoulder lift
-joint_goal[2] = np.deg2rad(105)      # elbow
-joint_goal[3] = np.deg2rad(-114)     # wrist1
-joint_goal[4] = np.deg2rad(-90)      # wrist2
-joint_goal[5] = np.deg2rad(60)       # wrist3
-group.go(joint_goal, wait=True)
-print("Reached Grap-Goal", joint_goal)
-
-"""pose_goal = geometry_msgs.msg.Pose()
-current_pose = group.get_current_pose()  # hole pose vom Arm
-
-# Arm TCP Goal setzen, !nicht für den Gripper
-current_pose = group.get_current_pose()  # hole pose vom Arm
-pose_goal = current_pose  # aktuelle Werte
-pose_goal.pose.position.z = pose_goal.pose.position.z - 0.02  # 2cm tiefer geht
-# -25cm, -20cm -8cm => ABORTED: No motion plan found. No execution attempted.
-# wegen Tischplatte? Ja, von -0,2 auf -0,22 gesetzt (Z89) => 8cm geht
-# 
-# new TCP goal for Group-ur5_arm set position:
-#  bei -15cm
-#  x: 0.30204676476308573
-#  y: 0.49030775998509935
-#  z: -0.11152023918351936
-# bei -10cm z: -0.061569993780283175
-print(" new TCP goal for Group-ur5_arm set", pose_goal.pose)
+input("confirm moving ur3_arm to this position")
 group.set_pose_target(pose_goal)
-plan = group.go(wait=True)
+plan = group.plan()
+sucess = group.go(wait=True)
+print("suc?", sucess)
 group.stop()
-group.clear_pose_targets()"""
+group.clear_pose_targets()
 
-# --- 5. Close the gripper
-input("\n Close Gripper => Enter")
-joint_gripper = group_gripper.get_current_joint_values()
-print("Gripper Angle is", joint_gripper)
-# complete open is 0.0007  closed is pi/4 =0.7854
-joint_gripper[0] = pi/11  # 0.07
-group_gripper.go(joint_gripper, wait=True)
-group_gripper.stop()
+# --- 5. turning gripper  - alleine Gelenkwinkel funkt nicht !!!
+# input("confirm turning wrist3")
+# joint_goal = group.get_current_joint_values()
+# joint_goal[5] = np.deg2rad(0)  # turn wrist3
+# group.go(joint_goal, wait=True)
 
-# --- 6. Move the TCP close to the object 0.2m up
-input(" Lift blue box => Enter")
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = np.deg2rad(-74)       # shoulder pan
-joint_goal[1] = np.deg2rad(-84)      # shoulder lift
-joint_goal[2] = np.deg2rad(98)      # elbow
-joint_goal[3] = np.deg2rad(-104)     # wrist1
-joint_goal[4] = np.deg2rad(-90)      # wrist2
-joint_goal[5] = np.deg2rad(60)       # wrist3
-group.go(joint_goal, wait=True)
-print("Reached TCP Goal above blue box", joint_goal)
-
-"""# ============== Cartesian Paths ==============
+# --- 6. go to grip position
 print("plan a cartesion path")
-# The Cartesian path is directly planned by specifying
-# the waypoints list through which the end effector passes
 waypoints = []
-
 wpose = group.get_current_pose().pose
-wpose.position.z = 0.2  # First move down (z)
+wpose.position.z -= 0.10  # move down (z)
 waypoints.append(copy.deepcopy(wpose))
-
-# We want the Cartesian path to be interpolated at a resolution of 1 cm
-# which is why we will specify 0.01 as the eef_step in Cartesian
-# translation.  We will disable the jump threshold
-# by setting it to 0.0 disabling:
 (plan, fraction) = group.compute_cartesian_path(
                                                 waypoints,
-                                                0.01,        # eef_step
+                                                0.001,        # eef_step
                                                 0.0)         # jump_threshold
-# Displaying a Trajectory
+# Displaying a Trajectory in RViZ
 display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 display_trajectory.trajectory_start = robot.get_current_state()
 display_trajectory.trajectory.append(plan)
-
 # Publish
 display_trajectory_publisher.publish(display_trajectory)
 
-# ==== Execute the calculated path:
-# print("the plan is", plan)
-print("execute plan ")
-group.execute(plan, wait=True)"""
+# Execute the calculated path:
+input("confirm moving ur3_arm 10 cm deeper")
+group.execute(plan, wait=True)
 
-# --- 7. Move the TCP above the plate
-# input(" Go to Home Position => Enter")
-joint_goal = group.get_current_joint_values()
-joint_goal[0] = np.deg2rad(60)       # shoulder pan
-joint_goal[1] = np.deg2rad(-46)      # shoulder lift
-joint_goal[2] = np.deg2rad(27)      # elbow
-joint_goal[3] = np.deg2rad(-71)     # wrist1
-joint_goal[4] = np.deg2rad(-91)     # wrist2
-joint_goal[5] = np.deg2rad(90)      # wrist3
+
+# --- 7. Gripping
+input("Confirm: you have to close the gripper with the UR3-Teach-Pad EA Werkzeugausgang 0 auf OFF")
+# für die  Trajektorienplanung das Object an den Gripper attachen
+grasping_group = "ur3_arm"
+touch_links = robot.get_link_names(group=grasping_group)
+print(touch_links)
+#  no worxxxxxxxxxxx  
+#  scene.attach_box('ee_link', box_name, touch_links=touch_links)
+
+
+# --- 8. go to home position
+input("confirm moving ur3_arm to home position")
+joint_goal = group.get_named_target_values("home")
 group.go(joint_goal, wait=True)
-print("Reached Goal above plate", joint_goal)
-
-# --- 8. Open the gripper
-# input("\n Open Gripper => Enter")
-joint_gripper = group_gripper.get_current_joint_values()
-print("Gripper Angle is", joint_gripper)
-joint_gripper[0] = 0.001  # complete open is 0.0007  closed is pi/4
-group_gripper.go(joint_gripper, wait=True)
-group_gripper.stop()
 
 # --- at the end -----
-# input("Remove Box")  # Otherwise it will stay
-#scene.remove_world_object(box_name)
-
-# API -Doku 
-# http://docs.ros.org/en/jade/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html#a6a4440597144e033cd1749e0e00716ca
-
-
-# Gripper Info
-# print(" received pose from arm ", current_pose)
-# ee_name = group_gripper.get_end_effector_link()  # hole Frame des EE
-# print(" ee ", ee_name)
-# current_pose = group_gripper.get_current_pose(end_effector_link=ee_name)
-# print(" received pose from ee is ", current_pose.pose)
