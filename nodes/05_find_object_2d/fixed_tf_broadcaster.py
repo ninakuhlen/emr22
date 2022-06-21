2d3d10f9a2484875809557ac03e1cb112acdbd31
Prof. Just <olaf.just@w-hs.de>
2022-06-21 14:03:06 +0200

    Update fixed_tf_broadcaster.py

--- nodes/05_find_object_2d/fixed_tf_broadcaster.py
+++ nodes/05_find_object_2d/fixed_tf_broadcaster.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
+
+# -----------------------------------------------------------------
+# Version vom 21.6.22
+
 # http://wiki.ros.org/tf/Tutorials/Adding%20a%20frame%20%28Python%29
 # usage:   $ rosrun tf tf_echo world object_8
-# -----------------------------------------------------------------
-# Version vom 13.6.22 mit numpy eul2quat
 
 import rospy
 import tf
 import numpy as np  # Scientific computing library for Python
+
 
 if __name__ == '__main__':
     rospy.init_node('fixed_tf_broadcaster')
     br = tf.TransformBroadcaster()
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
-        print("Transformation camera_link => object_8  send! ")
-        print(rospy.Time.now())
         # Hier Abstand camera_link zum world_frame eintragen
         # vorher messen in Meter
         # x, y, z    67cm hohes Stativ
-        # UR5e 13_06_22
-        br.sendTransform((1.30, 0.04, 0.70),
-                         (tf.transformations.quaternion_from_euler(0.0 , 0.5, 3.14)),
+        # quaternion( rool, picth, yaw)
+
+        # was br.sendTransform((-0.255, -0.47, 0.6),
+        br.sendTransform((-0.233, -0.43, 0.62),
+                         (tf.transformations.quaternion_from_euler(0, 0.80, 0.88, axes='sxyz')),
+                         #(tf.transformations.quaternion_from_euler(0, 0.88, 0.785, axes='sxyz')),
                          rospy.Time.now(),
                          "camera_link",
-                         "world")        
+                         "world")
+        print("sending transform /world => /camera_link")
+        print(rospy.Time.now())
         rate.sleep()
+
