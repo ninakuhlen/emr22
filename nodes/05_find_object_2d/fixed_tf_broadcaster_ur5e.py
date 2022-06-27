f496bfb0f7ba9f7e3f21bb75f58b20e3a95bc87d
Prof. Just <olaf.just@w-hs.de>
2022-06-27 14:42:55 +0200

    Create fixed_tf_broadcaster_ur5e.py

---
+++ nodes/05_find_object_2d/fixed_tf_broadcaster_ur5e.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# http://wiki.ros.org/tf/Tutorials/Adding%20a%20frame%20%28Python%29
+# usage:   $ rosrun tf tf_echo world object_8
+
+import rospy
+import tf
+import numpy as np  # Scientific computing library for Python
+
+if __name__ == '__main__':
+    rospy.init_node('fixed_tf_broadcaster')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        # Hier Abstand camera_link zum world_frame eintragen
+        # vorher messen in Meter
+        # x, y, z    67cm hohes Stativ
+        # quaternion?
+
+        # UR5e 13_06_22
+        br.sendTransform((1.30, 0.04, 0.62),
+                         (tf.transformations.quaternion_from_euler(0.0 , 0.5, 3.14)),
+                         rospy.Time.now(),
+                         "camera_link",
+                         "world")   
+        print("sending transform /world => /camera_link")
+        print(rospy.Time.now())     
+        rate.sleep()
