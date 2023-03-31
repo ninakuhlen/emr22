d5716e7e842db6098aebabfa45c2164345eb5d5c
Prof. Just <olaf.just@w-hs.de>
2023-03-31 09:20:27 +0200

    mit rqt-joint-trajectory-controller

--- install_script/step3_Install_UR_URDF_Gazebo.sh
+++ install_script/step3_Install_UR_URDF_Gazebo.sh
@@ -1,15 +1,15 @@
-#### Install the neede URDF for the UR5-Robot and Gazebo ###
+#### Install the needed URDF for the UR5-Robot and Gazebo ###
 
 # Install_UR_URDF_Gazebo.sh
 # ------------------------------------------------------------------
 # Installing 
 # auf einem Rechner mit Ubuntu 20.04 Focal Fossa  
 # OJ fuer robotik.bocholt@w-hs.de
-# SS2022
-# geaendert am 11.4.2022
+# SS2023
+# geaendert am 30.3.2023
 # vgl. https://ros-planning.github.io/moveit_tutorials/doc/getting_started/getting_started.html 
 
-echo -e "\033[34m ----- robotik.bocholt@w-hs.de -- SS22 - UR5 für Gazebo installieren.. ----- \033[0m "
+echo -e "\033[34m ----- robotik.bocholt@w-hs.de -- SS23 - UR5 für Gazebo installieren.. ----- \033[0m "
 # echo -e "\033[34m ----- ..und Workspace ws_moveit (catkin build) einrichten ---------- \033[0m "
 sudo apt-get-install ros-noetic-libuvc-ros
 
@@ -18,11 +18,11 @@ git clone https://github.com/ProfJust/universal_robot.git -b melodic-devel
 # Laut Issue #537 https://github.com/ros-industrial/universal_robot/issues/537 soll diese Paket auch für ROS-Noetic funktionieren. 
 
 # Gripper Robotiq
-git clone https://github.com/ProfJust/robotiq.git
+git clone https://github.com/ProfJust/robotiq.git -b gazebo
 
 
 # --- fertige MoveIt Config aus dem Assitenten --
-git clone https://github.com/ProfJust/ur5_gripper_moveit_config.git
+git clone https://github.com/ProfJust/ur5_gripper_moveit_config.git -b gazebo
 
 # --- ros_control --- 
 # UR5 Package
@@ -37,6 +37,7 @@ git clone https://github.com/ros-drivers/libuvc_ros.git
 # get camera vendor $ lsusb -v
 # start $ sudo -E rosrun libuvc_camera camera_node vendor:=...
 
+sudo apt-get install ros-noetic-rqt-joint-trajectory-controller
 rosdep update
 rosdep install --rosdistro $ROS_DISTRO --ignore-src --from-paths src
 
